60d127f8842a448d713201979189787dc2dcca0c
Tommy Chien <ad@tommychien.com>
2019-07-18 16:13:58 +0800

    update tokenizer

--- lensnlp/utils/tokenizer.py
+++ lensnlp/utils/tokenizer.py
@@ -1,37 +1,45 @@
 from typing import List
 import langid
+from lensnlp.utils.data import Token
+import jieba
+from segtok.segmenter import split_single
+from segtok.tokenizer import split_contractions
+from segtok.tokenizer import word_tokenizer
 
 
 class Tokenizer:
-    def __init__(self, sp_op: str = None):
+    def __init__(self, language_type: str = None, example: str = None, sp_op: str = None):
+
+        if language_type is None and example is None:
+            raise ValueError("Must specify language type or provides an example")
+
+        if sp_op is not None and sp_op not in ['char']:
+            raise ValueError("Not support the operation yet")
+
+        if language_type is not None:
+            self.language_type = language_type
+        else:
+            self.language_type = langid.classify(example)[0]
+
         self.sp_op = sp_op
 
-    def word_tokenizer(self, text) -> List[str]:
-        pass
+    def word_tokenizer(self, text) -> List[Token]:
+        tokenized = []
+        if self.language_type == 'zh':
+            if self.sp_op == 'char':
+                for index, char in enumerate(text):
+                    token = Token(char, start_position=index)
+                    tokenized.append(token)
+            else:
+                seg_list = list(jieba.tokenize(text))
+                for t in seg_list:
+                    token = Token(t[0], start_position=t[1])
+                    tokenized.append(token)
 
-    def sentence_split(self, text) -> List[str]:
-        pass
+        elif self.language_type == 'ug':
+            pass
 
-    if text is not None:
-        if language == 'UY':
-            text = uy_preprocess(text)
-            word = ''
-            for index, char in enumerate(text):
-                if char == ' ':
-                    if len(word) > 0:
-                        token = Token(word, start_position=index - len(word), lang=language)
-                        self.add_token(token)
-
-                    word = ''
-                else:
-                    word += char
-            index += 1
-            if len(word) > 0:
-                token = Token(word, start_position=index - len(word), lang=language)
-                self.add_token(token)
-
-        elif language == 'EN':
-
+        elif self.language_type == 'en':
             tokens = []
             sentences = split_single(text)
             for sentence in sentences:
@@ -61,18 +69,8 @@ class Tokenizer:
                 last_word_offset = running_offset - 1
                 last_token = token
 
-        elif language == 'CN_char':
-            for index, char in enumerate(text):
-                token = Token(char, start_position=index)
-                self.add_token(token)
+        return tokenized
 
-        elif language == 'CN_token':
-            seg_list = list(jieba.tokenize(text))
-            for t in seg_list:
-                token = Token(t[0], start_position=t[1])
-                self.add_token(token)
 
-        elif language == 'PY':
-            for index, char in enumerate(text):
-                token = Token(char, start_position=index, lang=language)
-                self.add_token(token)
+    def sentence_split(self, text) -> List[str]:
+        pass
