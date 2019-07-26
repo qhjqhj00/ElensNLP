eba4651fb1b33dc85b062042ce0d58ad1529a6e8
Tommy Chien <ad@tommychien.com>
2019-07-26 16:07:25 +0800

    0726

--- lensnlp/trainers/__init__.py
+++ lensnlp/trainers/__init__.py
@@ -1,2 +1,3 @@
 from .trainer import ModelTrainer
 from .re_trainer import ReTrainer
+from .seq2seq_trainer import Seq2SeqTrainer
--- lensnlp/trainers/seq2seq_trainer.py
+++ lensnlp/trainers/seq2seq_trainer.py
@@ -21,7 +21,7 @@ log = logging.getLogger('lensnlp')
 class Seq2SeqTrainer:
 
     def __init__(self,
-                 model: nn.Model,
+                 model: torch.nn.Module,
                  corpus: Seq2seqCorpus,
                  optimizer: Optimizer = Adam,
                  epoch: int = 0,
@@ -29,7 +29,7 @@ class Seq2SeqTrainer:
                  optimizer_state: dict = None,
                  ):
 
-        self.model: nn.Model = model
+        self.model = model
         self.corpus: Seq2seqCorpus = corpus
         self.optimizer: Optimizer = optimizer
         self.epoch: int = epoch
@@ -38,16 +38,13 @@ class Seq2SeqTrainer:
 
     def train(self,
               base_path: Union[Path, str],
-              learning_rate: float = 1.0,
-              mini_batch_size: int = 20,
+              learning_rate: float = 0.001,
+              mini_batch_size: int = 32,
               eval_mini_batch_size: int = 20,
               max_epochs: int = 150,
-              anneal_factor: float = 0.9,
-              train_with_dev: bool = False,
-              embeddings_in_memory: bool = True,
               test_mode: bool = False,
               **kwargs
-              ) -> dict:
+              ):
 
         if eval_mini_batch_size is None:
             eval_mini_batch_size = mini_batch_size
@@ -58,25 +55,16 @@ class Seq2SeqTrainer:
         init_log(log, base_path)
 
         log_line(log)
-        log.info(f'Evaluation method: Mircro F1-Score')
+        # log.info(f'Evaluation method: Mircro F1-Score')
 
         weight_extractor = WeightExtractor(base_path)
 
-        optimizer = self.optimizer(self.model.parameters(), lr=learning_rate, **kwargs)
+        optimizer = self.optimizer(self.model.parameters(), **kwargs)
 
         if self.optimizer_state is not None:
             optimizer.load_state_dict(self.optimizer_state)
 
         train_data = self.corpus.train
-
-        if train_with_dev:
-            train_data.extend(self.corpus.dev)
-
-        dev_score_history = []
-        dev_loss_history = []
-        train_loss_history = []
-
-        previous_metric = 0
 
         try:
 
@@ -111,8 +99,6 @@ class Seq2SeqTrainer:
                     seen_sentences += len(batch)
                     train_loss += loss.item()
 
-                    clear_embeddings(batch, also_clear_word_embeddings=not embeddings_in_memory)
-
                     if batch_no % modulo == 0:
                         log.info(f'epoch {epoch + 1} - iter {batch_no}/{len(batches)} - loss '
                                  f'{train_loss / seen_sentences:.8f}')
@@ -126,18 +112,6 @@ class Seq2SeqTrainer:
                 log_line(log)
                 log.info(f'EPOCH {epoch + 1} done: loss {train_loss:.4f} - lr {learning_rate:.4f}')
 
-                test_metric, test_loss = self._calculate_evaluation_results_for(
-                    'TEST', self.corpus.test, embeddings_in_memory, eval_mini_batch_size,
-                    base_path / 'test.tsv')
-                current_metric = test_metric.micro_avg_f_score()
-                if current_metric > previous_metric:
-                    self.model.save(base_path / 'best-model.pt')
-                    previous_metric = current_metric
-
-                learning_rate /= anneal_factor
-                for group in optimizer.param_groups:
-                    group['lr'] = learning_rate
-
             self.model.save(base_path / 'final-model.pt')
 
         except KeyboardInterrupt:
@@ -147,99 +121,4 @@ class Seq2SeqTrainer:
             self.model.save(base_path / 'final-model.pt')
             log.info('Done.')
 
-        return {'dev_score_history': dev_score_history,
-                'train_loss_history': train_loss_history,
-                'dev_loss_history': dev_loss_history}
-
-    def _calculate_evaluation_results_for(self,
-                                          dataset_name: str,
-                                          dataset: List[Sentence],
-                                          embeddings_in_memory: bool,
-                                          eval_mini_batch_size: int,
-                                          out_path: Path = None):
-
-        metric, loss = ReTrainer.evaluate(self.model, dataset, eval_mini_batch_size=eval_mini_batch_size,
-                                             embeddings_in_memory=embeddings_in_memory, out_path=out_path)
-
-        log.info(f'{dataset_name:<5}: loss {loss:.8f} - f-score {metric.micro_avg_f_score():.4f} '
-                 f'- acc {metric.macro_avg_f_score():.4f}')
-
-        return metric, loss
-
-    @staticmethod
-    def evaluate(model: nn.Model, data_set: List[Sentence],
-                 eval_mini_batch_size: int = 32,
-                 embeddings_in_memory: bool = True,
-                 out_path: Path = None) -> (
-            dict, float):
-        if isinstance(model, RelationExtraction):
-            return ReTrainer._evaluate_re_classifier(model, data_set,
-                                                     eval_mini_batch_size, embeddings_in_memory, out_path)
-
-    @staticmethod
-    def _evaluate_re_classifier(model: nn.Model,
-                                  sentences: List[Sentence],
-                                  eval_mini_batch_size: int = 20,
-                                  embeddings_in_memory: bool = False,
-                                  out_path: Path = None) -> (dict, float):
-
-        with torch.no_grad():
-            metric = Metric('Evaluation')
-
-            eval_loss = 0
-
-            batches = [sentences[x:x + eval_mini_batch_size] for x in
-                       range(0, len(sentences), eval_mini_batch_size)]
-
-            lines: List[str] = []
-            for batch in batches:
-
-                labels, loss = model.forward_labels_and_loss(batch)
-
-                clear_embeddings(batch, also_clear_word_embeddings=not embeddings_in_memory)
-
-                eval_loss += loss
-
-                sentences_for_batch = [sent.to_plain_string() for sent in batch]
-                confidences_for_batch = [[label.score for label in sent_labels] for sent_labels in labels]
-                predictions_for_batch = [[label.value for label in sent_labels] for sent_labels in labels]
-                true_values_for_batch = [sentence.get_label_names() for sentence in batch]
-                available_labels = model.label_dictionary.get_items()
-
-                for sentence, confidence, prediction, true_value in zip(sentences_for_batch, confidences_for_batch,
-                                                                        predictions_for_batch, true_values_for_batch):
-                    eval_line = '{}\t{}\t{}\t{}\n'.format(sentence, true_value[0], prediction[0], confidence[0])
-                    lines.append(eval_line)
-
-                for predictions_for_sentence, true_values_for_sentence in \
-                        zip(predictions_for_batch, true_values_for_batch):
-
-                    ReTrainer._evaluate_sentence_for_re_classification(metric,
-                                                                       available_labels,
-                                                                       predictions_for_sentence,
-                                                                       true_values_for_sentence)
-
-            eval_loss /= len(sentences)
-
-            if out_path is not None:
-                with open(out_path, "w", encoding='utf-8') as outfile:
-                    outfile.write(''.join(lines))
-
-            return metric, eval_loss
-
-    @staticmethod
-    def _evaluate_sentence_for_re_classification(metric,
-                                                 available_labels: List[str],
-                                                 predictions: List[str],
-                                                 true_values: List[str]):
-
-        for label in available_labels:
-            if label in predictions and label in true_values:
-                metric.add_tp(label)
-            elif label in predictions and label not in true_values:
-                metric.add_fp(label)
-            elif label not in predictions and label in true_values:
-                metric.add_fn(label)
-            elif label not in predictions and label not in true_values:
-                metric.add_tn(label)
 
